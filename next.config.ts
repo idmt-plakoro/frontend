eb816b79d3fef826afd8168aad9ede09c739e222
Thawin <bto2168@gmail.com>
2026-06-22 13:02:21 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,20 @@ const nextConfig: NextConfig = {
     },
     env: {
         BACKEND_URL: process.env.BACKEND_URL
+    },
+    async headers() {
+        return [
+            {
+                // matching all API routes or resource paths
+                source: "/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // change this to specific domain if needed
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ];
     }
 };
 
